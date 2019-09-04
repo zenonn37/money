93ced33e88a2b869437cb9f7a052ec08afc106c6
Christopher Crowd <codelife@Christophers-Mac-mini.local>
2019-09-04 19:12:25 -0400

    firt commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="app">
+    <main class="main-content">
+      <SideBar />
+      <div class="content">
+        <div class="container">
+          <router-view />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import SideBar from "@/components/SideBar";
+export default {
+  components: {
+    SideBar
   }
-}
-</style>
+};
+</script>
+
---
+++ src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <aside>
+    <div class="logo-box">
+      <div class="logo">MONEY</div>
+      <div class="hambuger">
+        <img src="../assets/menu.png" alt="menu" />
+      </div>
+    </div>
+
+    <div class="dynamic-btn">
+      <div class="btn">
+        <i class="fas fa-piggy-bank"></i>
+        <span>New Account</span>
+      </div>
+    </div>
+
+    <ul class="menu">
+      <li>
+        <a href>
+          <i class="fas fa-chart-line"></i>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href>
+          <i class="fas fa-chart-line"></i>
+          Wallet
+        </a>
+      </li>
+      <li>
+        <a href>
+          <i class="fas fa-chart-line"></i>
+          Transactions
+        </a>
+      </li>
+      <li>
+        <a href>
+          <i class="fas fa-chart-line"></i>
+          Settings
+        </a>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
