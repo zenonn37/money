9b29d34ac7adf7f1d3fbf469704d760d2674920e
Chris <christophercrowd@gmail.com>
2020-09-09 15:21:55 -0400

    updated account dash

--- src/views/accounts/Analytics.vue
+++ src/views/accounts/Analytics.vue
@@ -7,7 +7,39 @@
     <template v-else>
       <div>
         <div class="analytic-container">
-          <div class="mini-analytic">
+          <div class="overviewcard">
+            <div class="icon">
+              <h2>Debits</h2>
+            </div>
+            <div class="info">
+              <h1>25</h1>
+            </div>
+          </div>
+          <div class="overviewcard">
+            <div class="icon">
+              <h2>Credits</h2>
+            </div>
+            <div class="info">
+              <h1>8</h1>
+            </div>
+          </div>
+          <div class="overviewcard">
+            <div class="icon">
+              <h2>Spent</h2>
+            </div>
+            <div class="info">
+              <h1>3698.54</h1>
+            </div>
+          </div>
+          <div class="overviewcard">
+            <div class="icon">
+              <h2>balance</h2>
+            </div>
+            <div class="info">
+              <h1>1563.98</h1>
+            </div>
+          </div>
+          <!-- <div class="mini-analytic">
             <div class="analytic-row1">
               <Analytic :basic="ana_1" :integer="analytic.transactions" />
               <Analytic :basic="ana_2" :integer="analytic.debits" />
@@ -25,37 +57,40 @@
           <div class="trends">
             <div class="trends-panel">
               <div class="trend-control">
-                <apexchart
-                  height="100"
-                  :options="chartOptions"
-                  :series="series"
-                >
-                </apexchart>
+                <apexchart height="100" type="donut" :options="pie" :series=" pieData.series"></apexchart>
               </div>
-              <!-- <bars
-                :height="123"
-                :data="[
-                  { value: 0, title: '' },
-                  { value: 1, title: 'test1' },
-                  { value: 5, title: 'test5' },
-                  { value: 2, title: 'test2' },
-                  { value: 1, title: 'test1' },
-                ]"
-                :barWidth="5"
-                :gradient="['#6fa8dc', '#42b983']"
-                :growDuration="2"
-              >
-              </bars> -->
+              <div class="trend-control">
+                <apexchart type="pie" height="100" :options="pie2" :series="pieData2.series"></apexchart>
+              </div>
             </div>
           </div>
           <div class="reports">
-            <div class="reports-panel">Reports</div>
+            <div class="reports-panel">
+              <h2>Frequent Categories</h2>
+              <ul>
+                <li>
+                  <i class="fas fa-heartbeat"></i>
+                  <div>
+                    <h3>HealthCare</h3>
+                    <figure>Two transactions this month.</figure>
+                  </div>
+                  <div>
+                    <h2>$500.00</h2>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="chart">
             <div class="chart-container">
               <LineChart :charts="charts" />
             </div>
-          </div>
+          </div>-->
+        </div>
+        <div class="main-cards">
+          <div class="card">Card</div>
+          <div class="card">Card</div>
+          <div class="card">Card</div>
         </div>
       </div>
     </template>
@@ -72,87 +107,176 @@ export default {
   components: {
     Analytic,
     LineChart,
-    Loader,
+    Loader
   },
   data() {
     return {
-      chartOptions: {
-        chart: {
-          type: "bar",
-          width: 50,
-          height: 35,
-          sparkline: {
-            enabled: true,
-          },
-        },
-        plotOptions: {
-          bar: {
-            columnWidth: "80%",
-          },
-        },
-        labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
-        xaxis: {
-          crosshairs: {
-            width: 1,
-          },
-        },
+      pieData: {
+        series: [2000, 5550]
       },
-      series: [
-        {
-          name: "series-1",
-          data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54, 14, 15, 16, 17, 18],
+      pieData2: {
+        series: [1000, 4500]
+      },
+      pie: {
+        chart: {
+          type: "donut",
+          width: 30,
+          height: 30,
+          sparkline: {
+            enabled: true
+          }
         },
-      ],
+        labels: ["Spent", "Deposits"],
+        stroke: {
+          width: 1
+        },
+        dataLabels: {
+          enabled: true
+        },
+        tooltip: {
+          fixed: {
+            enabled: false
+          }
+        }
+      },
+      pie2: {
+        chart: {
+          type: "pie",
+          width: 30,
+          height: 30,
+          sparkline: {
+            enabled: true
+          }
+        },
+        colors: ["#30304d", "#b9fab8"],
+        labels: ["Spent", "Budget"],
+        stroke: {
+          width: 1
+        },
+        dataLabels: {
+          enabled: true
+        },
+        tooltip: {
+          fixed: {
+            enabled: false
+          }
+        }
+      },
+      bar: {
+        chartOptions: {
+          chart: {
+            type: "bar",
+            width: 50,
+            height: 35,
+            sparkline: {
+              enabled: true
+            }
+          },
+          plotOptions: {
+            bar: {
+              columnWidth: "80%"
+            }
+          },
+          labels: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            18
+          ],
+          xaxis: {
+            crosshairs: {
+              width: 1
+            }
+          }
+        },
+        series: [
+          {
+            name: "series-1",
+            data: [
+              25,
+              66,
+              41,
+              89,
+              63,
+              25,
+              44,
+              12,
+              36,
+              9,
+              54,
+              14,
+              15,
+              16,
+              17,
+              18
+            ]
+          }
+        ]
+      },
       loading: false,
       ana_1: {
         style: "analytic-panel",
         title: "Transactions",
         icon: "d_sign",
         background: "icon-circle basic",
-        cash: false,
+        cash: false
       },
       ana_2: {
         style: "analytic-panel",
         title: "Debits",
         icon: "trend_down",
         background: "icon-circle down",
-        cash: false,
+        cash: false
       },
       ana_3: {
         style: "analytic-panel",
         title: "Credits",
         icon: "trend_up",
         background: "icon-circle up",
-        cash: false,
+        cash: false
       },
       ana_4: {
         style: "analytic-panel-wide",
         title: "Spent",
         icon: "trend_up",
         background: "icon-circle up",
-        cash: true,
+        cash: true
       },
       ana_5: {
         style: "analytic-panel-wide",
         title: "Deposits",
         icon: "trend_up",
         background: "icon-circle up",
-        cash: true,
+        cash: true
       },
       ana_6: {
         style: "analytic-panel-wide",
         title: "Monthly Balance",
         icon: "trend_up",
         background: "icon-circle up",
-        cash: true,
+        cash: true
       },
       ana_7: {
         style: "analytic-panel-wide",
         title: "Daily Spending",
         icon: "trend_up",
         background: "icon-circle up",
-        cash: true,
-      },
+        cash: true
+      }
     };
   },
   computed: {
@@ -161,7 +285,7 @@ export default {
     },
     charts() {
       return this.$store.getters["transactions/GET_CHARTS"];
-    },
+    }
   },
   created() {
     const id = this.$route.params.account;
@@ -170,6 +294,6 @@ export default {
     this.$store.dispatch("transactions/monthly", id).then(() => {
       this.loading = false;
     });
-  },
+  }
 };
 </script>
